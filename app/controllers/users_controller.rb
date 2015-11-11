0266622706508e4483284d156dfcdda7180739f8
Barnabas Bulpett <bbulpett@charter.net>
2015-11-10 19:41:01 -0500

    Add strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,15 @@ class UsersController < ApplicationController
 	    render "new"
 	  end
 	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
